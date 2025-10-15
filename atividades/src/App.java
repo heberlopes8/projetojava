5eb17bfa34920102352486e89c164a848e8b1ee6
heberlopes8 <127308561+heberlopes8@users.noreply.github.com>
2025-10-15 22:40:23 +0000

    java

---
+++ atividades/src/App.java
@@ -0,0 +1,13 @@
+// Atividade 2: Controle de Estoque de Produtos
+//Desenvolva uma classe produto com:
+// ~Atributos privados:
+//1. nome (String)
+//2. codigo(String)
+//3 quantidade (int)
+//4. precoUnitario(double)
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
