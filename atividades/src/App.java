7cb1644f29b13fc4ce07d74ebad79e49ee0d591e
heberlopes8 <127308561+heberlopes8@users.noreply.github.com>
2025-10-16 00:29:18 +0000

    Terminar código

--- atividades/src/App.java
+++ atividades/src/App.java
@@ -5,9 +5,24 @@
 //2. codigo(String)
 //3 quantidade (int)
 //4. precoUnitario(double)
-
+// metodos
+// construtor padrao
+//contrutor com todos os parametros
+//metodo adicionar Estoque(int quantidade) que incremente a quantidade
+// metodo remover Estoque(int quantidade) que decremente a quantidade
+//crie um programa com menu dowhile para 
+//1 cadrastgrar novo produto
+//2 adicionar estoque
+//3 Remover estoque 
+//4 Calcular valor total do estoque
+//5 sair do programa
+import java.util.Scanner;
+import exercicio2.produto;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+       
+        Produtos produtos1 = new Produtos("nome", "codigo", 5, 30);
+    
+    
     }
 }
---
+++ atividades/src/exercicio2/produtos.java
@@ -0,0 +1,33 @@
+
+package exercicio2;
+
+public class produtos {
+        private String nome;
+        private String codigo;
+        private int quantidade;
+        private double precoUnitario;
+
+            public produtos(String nome, String codigo, int quantidade, double precoUnitario){
+                this.nome = nome;
+                this.codigo = codigo;
+                this.quantidade = quantidade;
+                this.precoUnitario = precoUnitario;
+                }
+                public String getnome(){return nome;}
+                public void setnome(String nome){this.nome =nome;}
+                public String getcodigo(){return codigo;}
+                public void setcodigo(String codigo) {this.codigo = codigo;}
+                public int getquantidade(){return quantidade;}
+                public void setcodigo(int quantidade) {this.quantidade = quntidade;}
+                public double getprecoUnitario(){return precoUnitario;}
+                public void setprecoUnitario(return precoUnitario) {this.precoUnitario = precoUnitario;}
+                
+                public double adicionarEstoque (){
+                double quantidade = 5.0
+                 
+                return 
+
+
+            }
+
+}
